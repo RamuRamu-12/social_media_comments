1206ef9e72e03af6468baced46c31b976ff13586
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2025-02-21 10:46:13 +0530

    Update urls.py

--- Hana_app/Hana/urls.py
+++ Hana_app/Hana/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('txt_response', gen_txt_response, name='processing_files'),
-    path('graph response', gen_graph_response, name='query system'),
-    path('txt_graph_response', gen_response, name='query system'),
+#     path('graph response', gen_graph_response, name='query system'),
+#     path('txt_graph_response', gen_response, name='query system'),
 ]

d20ca420ebd316e48737028a0e52c1e04d33fa29
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2025-02-21 11:08:00 +0530

    Update settings.py

--- Hana_app/Hana_app/settings.py
+++ Hana_app/Hana_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vae7@jegwjidpuvdk7&=*ozosm(w03cxuxi@5k&j8hr&#*_)vg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

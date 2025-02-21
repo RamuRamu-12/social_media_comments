e6851dc6fb73332c2972a9df3d95223693a0c9e2
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2025-02-21 11:38:48 +0530

    Update settings.py

--- Hana_app/Hana_app/settings.py
+++ Hana_app/Hana_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Hana',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Hana_app.urls'
@@ -69,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Hana_app.wsgi.application'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database

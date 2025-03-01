f1216d8c9cbd91bd72273d3695e7f9226de2646a
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2025-03-01 16:03:43 +0530

    Update sla_urls

--- Hana_app/Hana/urls.py
+++ Hana_app/Hana/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('txt_response', gen_txt_response, name='processing_files'),
-#     path('graph response', gen_graph_response, name='query system'),
-#     path('txt_graph_response', gen_response, name='query system'),
+    path('sla_data_process', process_query, name='processing_files'),
+    path('sla_query_making', sla_query, name="sla_query"),
 ]
